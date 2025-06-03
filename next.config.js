d42f2bedc860dea248db7222f5a4e2cc3f942bdf
Abdul Nafay <abdulnafay1652022@gmail.com>
2025-06-03 20:15:32 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/scrptblewebsite' : '',
-}
+   /** @type {import('next').NextConfig} */
+   const isGithubPages = process.env.GITHUB_PAGES === 'true';
 
-module.exports = nextConfig 
+   const nextConfig = {
+     reactStrictMode: true,
+     swcMinify: true,
+     output: 'export',
+     images: {
+       unoptimized: true,
+     },
+     basePath: isGithubPages ? '/scrptblewebsite' : '',
+   };
+
+   module.exports = nextConfig;
